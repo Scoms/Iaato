12434d9298f19d4c0ef6a0a46bcf86ff9e546d61
Toomz <eleonore.pirri@gmail.com>
2013-02-27 11:05:15 +0100

    un petit peu de html propre...

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
-<?php
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 4.1 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
-echo '<h1>Welcome, to the real World</h1>';
-echo '<h2>Je suis là !!!</h2>';
+<html>
+	<head>
+		<link rel="stylesheet" media='screen' title='Style' href="style.css">
+		<meta http-equiv="Content-Type" content="text/html" charset="utf8" />
+		<title>Iaato</title>
+	</head>
+	<body>
+		<h1>Welcome, to the real World</h1>
+		<h2>Je suis là !!!</h2>
+		
+	</body>
+</html>
 
-?>
