fe251b4461ff318aff284cb152b0822628abba20
Scoms <wieser.laurent@gmail.com>
2013-03-18 10:29:46 +0100

    formulaire d'identifiaction

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Sdz\BlogBundle\SdzBlogBundle(),
             new Iaato\IaatoBundle\IaatoIaatoBundle(),
+            new Iaato\UserBundle\IaatoUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
