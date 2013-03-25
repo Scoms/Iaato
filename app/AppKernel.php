87af0de29fda3b616af8ee9ad2218cc8ab7d02e4
Scoms <wieser.laurent@gmail.com>
2013-03-25 10:28:40 +0100

    refresh

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,7 +19,6 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-            new Sdz\BlogBundle\SdzBlogBundle(),
             new Iaato\IaatoBundle\IaatoIaatoBundle(),
             new Iaato\UserBundle\IaatoUserBundle(),
             //new FOS\UserBundle\FOSUserBundle(),
