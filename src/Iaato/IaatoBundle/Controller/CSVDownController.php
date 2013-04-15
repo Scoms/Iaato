a2a733225262b7295eb29227ebda5d6c807e87fe
Toomz <eleonore.pirri@gmail.com>
2013-04-15 15:18:52 +0200

    CSV

--- src/Iaato/IaatoBundle/Controller/CSVDownController.php
+++ src/Iaato/IaatoBundle/Controller/CSVDownController.php
@@ -5,14 +5,26 @@ namespace Iaato\IaatoBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use JMS\SecurityExtraBundle\Annotation\Secure;
-use Iaato\IaatoBundle\Entity\Society;
 use Symfony\Component\Security\Core\SecurityContext;
 
 class CSVDownController extends Controller
 {
   public function indexAction()
-  {
-    return $this->render('IaatoIaatoBundle:CSV:download.html.twig');
+  {  
+    $repository = $this->getDoctrine()->getRepository('IaatoIaatoBundle:Activity'); 
+    $query = $repository->createQueryBuilder('s'); 
+    $query->orderBy('s.id', 'DESC'); 
+
+    $data = $query->getQuery()->getResult(); 
+    $filename = "export_".date("Y_m_d_His").".csv"; 
+
+    $response = $this->render('IaatoIaatoBundle:CSV:download.html.twig'); 
+    $response->headers->set('Content-Type', 'text/csv');
+
+    $response->headers->set('Content-Disposition', 'attachment; filename='.$filename); 
+    return $response; 
+    
+	//return $this->render('IaatoIaatoBundle:CSV:download.html.twig');
   }
 }
 
--- src/Iaato/IaatoBundle/Controller/CSVUpController.php
+++ src/Iaato/IaatoBundle/Controller/CSVUpController.php
@@ -12,8 +12,51 @@ class CSVUpController extends Controller
 {
   public function indexAction()
   {
-    return $this->render('IaatoIaatoBundle:CSV:upload.html.twig');
+	  $form = $this->createFormBuilder()
+		->add('file', 'file')
+		->getForm();
+
+		$request = $this->get('request');
+
+
+		// Check if we are posting stuff
+		if ($request->getMethod() == 'POST') {
+			
+			$form->bind($request);
+			
+			if ($form->isValid()) {
+				
+				
+				$file = $form->get('file');
+                $filename = $form->getOriginalName();
+//                list($filename, $extension) = explode('.', $file);
+
+				// Your csv file here when you hit submit button
+				$fichier = file($file->getData());
+				
+				$response = $this->forward('IaatoIaatoBundle:CSVUp:show', array('file'  => $fichier , 'filename' => $filename));
+				return $response;
+				//return $this->redirect($this->generateUrl('iaato_csv_show', array('file' => $fichier)));
+				//return $this->render('IaatoIaatoBundle:CSV:show.html.twig', array('data' => 'test'));
+			}
+		}
+		return $this->render('IaatoIaatoBundle:CSV:upload.html.twig', array('form' => $form->createView()));
   }
+  
+  public function showAction($file, $filename)
+  {
+	$total = count($file);
+	$data = array();
+
+	for($i = 1; $i < $total; $i++){
+		$lin = $file[$i];
+		list($varA, $varB) = explode(';', $lin);
+		$data[] = array($varA, $varB);
+	}
+	
+	return $this->render('IaatoIaatoBundle:CSV:show.html.twig', array('data' => $data , 'filename' => $filename));
+  }
+  
 }
 
 ?>
