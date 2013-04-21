c035024fcb1b28baa052cdf93da6a993a0068f66
Toomz <eleonore.pirri@gmail.com>
2013-04-21 23:06:31 +0200

    Ship and Society

--- src/Iaato/IaatoBundle/Controller/CSVUpController.php
+++ src/Iaato/IaatoBundle/Controller/CSVUpController.php
@@ -15,6 +15,7 @@ use Symfony\Component\Security\Core\SecurityContext;
 
 class CSVUpController extends Controller
 {
+  
   public function indexAction()
   {
       
@@ -35,24 +36,95 @@ class CSVUpController extends Controller
                 $data = $file->getData();
                 
                 $filename = $data->getClientOriginalName();
-                echo $filename;
-                
-				$response = $this->forward('IaatoIaatoBundle:CSVUp:load', array('data'  => $data, 'filename' => $filename));
-				return $response;
+                $name = explode(".", $filename);
+                switch($name[0]){
+                    case "ships":
+                        return $this->forward('IaatoIaatoBundle:CSVUp:ship', array('data' => $data));
+                        break;
+                    case "society":
+                        return $this->forward('IaatoIaatoBundle:CSVUp:society', array('data' => $data));
+                        break;
+                    default:
+                        break;
+                }
 			}
 		}
 		return $this->render('IaatoIaatoBundle:CSV:upload.html.twig', array('form' => $form->createView()));
         
   }
   
-  public function loadAction($data, $filename)
+  
+  
+  public function shipAction($data)
   {
-    
     $handle = fopen($data, "r");
     while(($lin = fgetcsv($handle,1000,";")) !== FALSE){
         
         $em = $this->getDoctrine()->getEntityManager();
         $ship = new Ship();
+        $ship->setNameShip($lin[0]);
+        $ship->setCode($lin[3]);
+        $ship->setNbPassenger($lin[6]);
+        
+        if($em->getRepository("IaatoIaatoBundle:Society")->findOneBy(array('labelSociety' => $lin[1]))!=NULL){
+            $society = $em->getRepository("IaatoIaatoBundle:Society")->findOneBy(array('labelSociety' => $lin[1]));
+            $ship->setSociety($society);
+        }
+        
+        if($em->getRepository("IaatoIaatoBundle:Type")->findOneBy(array('labelType' => $lin[2]))!=NULL){
+            $type = $em->getRepository("IaatoIaatoBundle:Type")->findOneBy(array('labelType' => $lin[2]));
+            $ship->setType($type);
+        }
+        else{
+            $type = new Type();
+            $type->setLabelType($lin[2]);
+            $em->persist($type);
+            $ship->setType($type);
+        }
+        
+        if($em->getRepository("IaatoIaatoBundle:Email")->findOneBy(array('email' => $lin[4]))!=NULL){
+            /*
+             * L'email existe déjà donc il doit déjà être attribué à un autre bateau... donc PROBLEME
+            */
+            
+        }
+        else{
+            $email = new Email();
+            $email->setEmail($lin[4]);
+            $email->setShip($ship);
+            $em->persist($email);
+            $ship->addEmail($email);
+        }
+        
+        if($em->getRepository("IaatoIaatoBundle:Phone")->findOneBy(array('numberPhone' => $lin[5]))!=NULL){
+            /*
+             * Le phone existe déjà donc il doit déjà être attribué à un autre bateau... donc PROBLEME
+            */
+        
+        }
+        else{
+            $phone = new Phone();
+            $phone->setNumberPhone($lin[4]);
+            $phone->setShip($ship);
+            $em->persist($phone);
+            $ship->addPhone($phone);
+        }
+            
+        $em->persist($ship);
+        $em->flush();
+        
+    }
+    fclose($handle);
+	return $this->render('IaatoIaatoBundle:CSV:show.html.twig');
+  }
+  
+  public function societyAction($data)
+  {
+    $handle = fopen($data, "r");
+    while(($lin = fgetcsv($handle,1000,";")) !== FALSE){
+        
+        $em = $this->getDoctrine()->getEntityManager();
+        $soc = new Society();
         $ship->setNameShip($lin[0]);
         $ship->setCode($lin[3]);
         $ship->setNbPassenger($lin[6]);
