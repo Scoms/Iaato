d5aa6929290f331b1913f722e27c3df908a39e76
Toomz <eleonore.pirri@gmail.com>
2013-05-28 15:40:41 +0200

    map

--- web/javascripts/gmap.js
+++ web/javascripts/gmap.js
@@ -18,29 +18,66 @@ $(function() {
   });
   //addSites(-62.24, 59.47);
   //addSites(-64.14, 61.08);
+
+  var lineSymbol = {
+    path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW
+  };
+
   polyline = new google.maps.Polyline({
     strokeColor:   'red', //on définit la couleur
     strokeOpacity: 0.5,       //l'opacité
-    strokeWeight:  3,         //l'épaisseur du trait,
+    strokeWeight:  2,         //l'épaisseur du trait,
+    icons: [{
+      path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW
+      //icon: lineSymbol,
+      offset: '15',
+      fillColor: 'green',
+    }],
     map:           gmap       //la map à laquelle rattacher la polyline
   });
   path = polyline.getPath();
+  
+  var sites = new Array();
+  var array_content = new Array();
+  for(var cor in list_step){
+      var name = cor[2];
+      if($.inArray(name, sites) == -1){
+        var content = "<div><strong>"+cor[2]+"</strong><br/><p>"+cor[3]+"</p>";
+        /*
+        array_content[name] = "<div><strong>"+cor[2]+"</strong><br/>";
+        array_content[name] += "<p>"+cor[3]+"</p>";
+        */
+        array_content.push(content);
+        sites.push(name);
+      }
+      else{
+        array_content[name] += "<p>"+cor[3]+"</p>";
+      }
+      
+  }
+  
+  
+
+  //cpt = 0;
   for(var coor in list_step)
   {
-      addSites(list_step[coor]);
+      addSites(list_step[coor], array_content[coor[2]]); 
+      //cpt+=1;  
   }
 });	
 
-function addSites(coor)
+function addSites(coor, content)
 {
     var myLatlng = new google.maps.LatLng(-coor[0],-coor[1]);
     path.push(myLatlng);
     var marker = new google.maps.Marker({ position: myLatlng, title: coor[2]});
     marker.setMap(gmap);
-    var content = "<div><strong>" + coor[2] + "</strong><br/>";
+    /*
+    var content = "<div><strong>"+coor[2]+"</strong><br/>";
     content += "<p>"+coor[3]+"</p>";
+    */
     content += "<p>"+coor[0]+";"+coor[1]+"</p></div>";
-    var infowindow = new google.maps.InfoWindow({ content: content });
+    var infowindow = new google.maps.InfoWindow({ content: content } );
     gMarkers.push(new GMarkerClass(marker, infowindow));  
 }
 
